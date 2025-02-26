3dc4040fa00650dd4f039c0726aa05e0d7dae6c7
Daniele Cruciani <daniele@smartango.com>
2025-02-26 17:38:07 +0100

    completion for shell

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{generate_to, Shell};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    name: Option<String>,
-    version: Option<String>,
-    about: Option<String>,
-    long_about: Option<String>,
 
     /// Turn debugging information on
     #[arg(short, long, action = clap::ArgAction::Count)]
@@ -16,6 +13,7 @@ struct Cli {
 
     #[command(subcommand)]
     command: RunMode,
+
 }
 
 #[derive(Subcommand)]
@@ -25,24 +23,30 @@ enum RunMode {
         #[arg(short, long)]
         set_server: String,
     },
-
+    
     #[clap(name = "proc-define")]
     ProcDefine,
-
+    
     #[clap(name = "proc-commit")]
     ProcCommit{
         name: String,
         #[arg(short, long)]
         filename: PathBuf,
     },
-
+    
     #[clap(name = "role-subscription")]
     RoleSubscription, // interactive TUI mode
-
+    
     #[clap(name = "role-subrequest")]
     RoleSubrequest {
         filename: PathBuf,
         keyfilename: PathBuf,
+    },
+
+    #[clap(name = "completion")]
+    Completion {
+        shell: Option<Shell>,
+        out_dir: Option<PathBuf>,
     },
 }
 
@@ -65,6 +69,36 @@ fn main() {
         RunMode::RoleSubrequest{ filename, keyfilename} => {
             println!("RoleSubrequest: {:?} {:?}", filename, keyfilename);
         }
+        RunMode::Completion{shell: Some(shell), out_dir} => {
+            let mut cmd = Cli::command();
+            let out_dir = if let Some(d) = out_dir {
+                d
+            } else {
+                &PathBuf::from(".")
+            };
+            match shell {
+                clap_complete::Shell::Bash => {
+                    generate_to(Shell::Bash, &mut cmd, "nopuma-cli", out_dir).unwrap();
+                }
+                clap_complete::Shell::Fish => {
+                    generate_to(Shell::Fish, &mut cmd, "nopuma-cli", out_dir).unwrap();
+                }
+                clap_complete::Shell::Zsh => {
+                    generate_to(Shell::Zsh, &mut cmd, "nopuma-cli", out_dir).unwrap();
+                }
+                clap_complete::Shell::PowerShell => {
+                    generate_to(Shell::PowerShell, &mut cmd, "nopuma-cli", out_dir).unwrap();
+                }
+                clap_complete::Shell::Elvish => {
+                    generate_to(Shell::Elvish, &mut cmd, "nopuma-cli", out_dir).unwrap();
+                },
+                &_ => {
+                    println!("Unsupported shell");
+                }
+            }
+        },
+        RunMode::Completion{shell: None, out_dir: _} => {
+            println!("shell not provided");
+        }
     }
-
 }
